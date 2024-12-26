acc8540aaea088de4cd7f9ae177a62606c0814f7
shrishail madari <shrishailmadari@gmail.com>
2024-12-26 16:20:38 +0530

    factory design pattern.

--- CreationDesignPatterns/src/factorymethod/example1/ClientCode.java
+++ CreationDesignPatterns/src/factorymethod/example1/ClientCode.java
@@ -1,6 +1,47 @@
 package factorymethod.example1;
 interface FileGenerator{
+    void generateFile();
+}
+class PdfGenerator implements FileGenerator{
 
+    @Override
+    public void generateFile() {
+        System.out.println("Generating PDF files");
+    }
+}
+class CsvFileGenerator implements FileGenerator{
+
+    @Override
+    public void generateFile() {
+        System.out.println("generating Csv files");
+    }
+}
+class DockFileGenerator implements FileGenerator{
+
+    @Override
+    public void generateFile() {
+        System.out.println("generating the Document file");
+    }
+}
+class FileGeneratorFactory{
+    public FileGenerator createFiles(String fileType){
+        switch (fileType){
+            case "Pdf":
+                return new PdfGenerator();
+            case "Csv":
+                return new CsvFileGenerator();
+            case "Docs":
+                return new DockFileGenerator();
+            default:
+                throw new IllegalArgumentException("unknown file type"+fileType);
+        }
+
+    }
 }
 public class ClientCode {
+    public static void main(String[] args) {
+        FileGeneratorFactory fileGeneratorFactory = new FileGeneratorFactory();
+        FileGenerator pdf = fileGeneratorFactory.createFiles("Pdf");
+        pdf.generateFile();
+    }
 }
